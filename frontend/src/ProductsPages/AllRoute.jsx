ee16bd844fa1558cf5995899c39d5a423bb54966
Sanket yadav <yadavsanket24@gmail.com>
2022-12-14 18:20:13 +0530

    added  Home page

--- frontend/src/ProductsPages/AllRoute.jsx
+++ frontend/src/ProductsPages/AllRoute.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import ClothesSingle from './ClothesSingle'
+import Homepage from "../pages/Homepage";
+import Login from "../Components/Login";
 
 const AllRoute  = () => {
   return (
     <Routes>
         <Route path="/cloth/:id"   element={<ClothesSingle/>}/>
+        <Route path="/" element={<Homepage />}></Route>
+        <Route path="/login" element={<Login />}></Route>
     </Routes>
   )
 }
---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../Components/Footer'
+import Home from '../Components/Home'
+import Navbar from '../Components/Navbar'
+
+const Homepage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Home/>
+        <Footer />
+    </div>
+  )
+}
+
+export default Homepage
