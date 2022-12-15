58203583dfb4c4b19c33d943c258f750c6529246
Sanket yadav <yadavsanket24@gmail.com>
2022-12-16 00:34:10 +0530

    added raw

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import AllRoute from "./ProductsPages/AllRoute"
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar/> */}
       <AllRoute/>
+      <Footer/>
     </div>
   );
 }
