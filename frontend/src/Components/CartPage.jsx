a28ebf470bf434ce19d07461d82ea398cff56916
Pratik Kumar <khoonipratik@gmail.com>
2022-12-15 09:11:40 +0530

    removing unnecessary part

--- frontend/src/Components/CartPage.jsx
+++ frontend/src/Components/CartPage.jsx
@@ -7,8 +7,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 
 const CartPage = () => {
