914f22ce1a3f48d1a1c34ba912fcfe4d5c61e520
Sanket yadav <yadavsanket24@gmail.com>
2022-12-16 20:27:56 +0530

    latest changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,6 @@ function App() {
     <div className="App">
 
      <CartPage/>
-
        <Navbar/> 
       <AllRoute/>
       <Footer/>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,23 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-
+import { BrowserRouter } from "react-router-dom";
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-       <ChakraProvider>
+   //     <ChakraProvider>
+   //      <App />
+   //   </ChakraProvider>
+   <Provider store={store}>
+    <BrowserRouter>
+      <ChakraProvider>
         <App />
-     </ChakraProvider>
+      </ChakraProvider>
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
