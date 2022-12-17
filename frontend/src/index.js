4c231b7660460adcdcc43711e1c053048a291353
Pratik Kumar <khoonipratik@gmail.com>
2022-12-17 11:05:31 +0530

    changes kkkkk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"
 import { store } from './Redux/store';
 import { Provider } from 'react-redux';
 
